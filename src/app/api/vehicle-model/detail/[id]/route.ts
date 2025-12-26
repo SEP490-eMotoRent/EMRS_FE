1773ed947b680cf45aea0b5c3dd5af1b0749bc9f
HoangAnh15062002 <hoanganhzt1506@gmail.com>
2025-12-26 16:41:10 +0700

    Update route.ts

--- src/app/api/vehicle-model/detail/[id]/route.ts
+++ src/app/api/vehicle-model/detail/[id]/route.ts
@@ -26,7 +26,6 @@ export async function GET(
       headers: { Authorization: `Bearer ${token}` },
     });
     const text = await beRes.text();
-    );
 
     return new NextResponse(text, {
       status: beRes.status,
